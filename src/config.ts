483244a22cb6534b008b3835d2585a30738b01a7
ᔆ ᴾ ᴱ ᴱ ᴰ ™ ⌁ 🇵🇸 <roroxroot@gmail.com>
2026-02-10 13:58:12 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { ServerOptions } from './types/ServerOptions';
 
 export default {
-  secretKey: 'THISISMYSECURETOKEN',
+  secretKey: '5oEVRGDGaiVbEFtL',
   host: 'http://localhost',
   port: '21465',
   deviceName: 'WppConnect',
